56ffa545ad095b46fbe3a70d08b17293f0966ace
sadwhale09 <sadwhale09@github.com>
2022-12-09 22:09:46 +0100

    Change arrow glyphs in nvim-tree

--- lua/user/plugins/nvim-tree.lua
+++ lua/user/plugins/nvim-tree.lua
@@ -14,8 +14,10 @@ nvimtree.setup({
         icons = {
             glyphs = {
                 folder = {
-                    arrow_closed = "", -- arrow when folder is closed
-                    arrow_open = "", -- arrow when folder is open
+                    arrow_closed = ">", -- arrow when folder is closed
+                    arrow_open = "v", -- arrow when folder is open
+                    -- arrow_closed = "", -- arrow when folder is closed
+                    -- arrow_open = "", -- arrow when folder is open
                 },
             },
         },
