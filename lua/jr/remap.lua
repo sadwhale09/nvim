65ff7c669ae00403bb845beb0eceec451947a147
sadwhale09 <dev.jraniszewski@pm.me>
2023-05-18 17:07:12 +0200

    Map Escape as :nohl; hlsearch=true

--- lua/jr/remap.lua
+++ lua/jr/remap.lua
@@ -37,7 +37,7 @@ vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 
 -- Clear search highlight
-vim.keymap.set('n', '<leader>nh', ':nohl<CR>')
+vim.keymap.set('n', '<Esc>', ':nohl<CR>')
 
 -- Deleted single character does not go to the copy register
 vim.keymap.set('n', 'x', '"_x')
