fda081dcec800fff8343823e6b69a60e662b7d98
sadwhale09 <dev.jraniszewski@pm.me>
2024-01-18 12:20:48 +0100

    Remove commented-out funciton for executing python files

--- init.lua
+++ init.lua
@@ -661,33 +661,6 @@ cmp.setup({
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
--- -- Shortcut to execute python files
--- local function run_curr_python_file()
---     -- Get file name in the current buffer
---     local file_name = vim.api.nvim_buf_get_name(0)
---
---     -- Get terminal codes for running python file
---     -- ("i" to enter insert before typing rest of the command)
---     local py_cmd = vim.api.nvim_replace_termcodes("ipython \"" .. file_name .. "\"<cr>", true, false, true)
---
---     -- Determine terminal window split and launch terminal
---     local percent_of_win = 0.4
---     local curr_win_height = vim.api.nvim_win_get_height(0) -- Current window height
---     local curr_win_width = vim.api.nvim_win_get_width(0) -- Current window width
---     local term_height = math.floor(curr_win_height * percent_of_win) -- Terminal height
---     local term_width = math.floor(curr_win_width * 0.5) -- Terminal height
---     -- vim.cmd(":below " .. term_height .. "split | term") -- Launch terminal (horizontal split)
---     vim.cmd(":below " .. term_width .. "vsplit | term") -- Launch terminal (vertical split)
---
---     -- Press keys to run python command on current file
---     vim.api.nvim_feedkeys(py_cmd, "t", false)
--- end
---
--- vim.keymap.set({'n'}, '<F4>', '', {
---     desc = "Run .py file via Neovim built-in terminal",
---     callback = run_curr_python_file
--- })
-
 -- Execute various filetypes:
 local function save_and_exec()
 	local function run_term(command, ...)
