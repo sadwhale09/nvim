7dbf672720ba0c1477912f0c7a715d7935469f19
sadwhale09 <dev.jraniszewski@pm.me>
2024-05-29 14:02:05 +0200

    Same with ":LazyGit"

--- init.lua
+++ init.lua
@@ -137,7 +137,7 @@ require("lazy").setup({
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
-		vim.keymap.set("n", "<leader>lg", " :LazyGit<CR>", { desc = "[L]azy [G]it" }),
+		vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", { desc = "[L]azy [G]it" }),
 	},
 
 	-- Useful plugin to show you pending keybinds.
