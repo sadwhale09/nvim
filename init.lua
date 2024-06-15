8f16186d0a82837725ae4368ce9c5b229219a272
sadwhale09 <dev.jraniszewski@pm.me>
2024-06-15 22:50:06 +0200

    Move lines with leader to remap

--- init.lua
+++ init.lua
@@ -1,13 +1,6 @@
 require("options")
 require("remap")
 
---]]
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
--- remap.lua
+++ remap.lua
@@ -1,3 +1,10 @@
+--]]
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- [[ Basic Keymaps ]]
 
 -- Better navibation between windows
